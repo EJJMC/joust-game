6d0b1dfba87b90d18ba94dd8ca884addc0ff97be
Caoimhe <d22126064@mytudublin.ie>
2022-10-19 11:52:41 +0100

    Tweaked Player Movement

--- 2d test/Assets/PlayerMovment.cs
+++ 2d test/Assets/PlayerMovment.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour
-
 {
-    public float movmentspeed = 2;
-    public float jumpforce = 1;
-    private Rigidbody2D _rigidbody;
+    private Rigidbody2D rb;
+
+    private float dirX = 0f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        var movment = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movment,0,0) * Time.deltaTime * movmentspeed;
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 9f, rb.velocity.y);
 
-        
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        if (Input.GetButtonDown("Jump"))
         {
-            _rigidbody.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
         }
-        
     }
 }
