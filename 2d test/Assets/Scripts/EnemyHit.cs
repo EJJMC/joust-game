20713697e0e6e7cbce8046c13b28f57d2329dfe4
EJJMC <eoinjmcsharry@gmail.com>
2022-10-20 20:18:11 +0100

    player can kill now with pickup drop

---
+++ 2d test/Assets/Scripts/EnemyHit.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHit : MonoBehaviour
+{
+
+    public GameObject egg;
+    public GameObject enemy;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     egg.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(enemy);
+            egg.SetActive(true);
+        }
+    }
+}
