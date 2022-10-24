271a9d61a652c1a825195309865fbba2f2eb6667
EJJMC <eoinjmcsharry@gmail.com>
2022-10-24 15:12:03 +0100

    added win condition

--- 2d test/Assets/Scripts/LevelScripts/ItemCollector.cs
+++ 2d test/Assets/Scripts/LevelScripts/ItemCollector.cs
@@ -1,14 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
+    public TMP_Text score;
+    public int scoreValue = 0;
+
+    private void Update()
+    {
+        endGame();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Collectable"))
         {
             Destroy(collision.gameObject);
+            scoreValue += 1;
+            SetScore();
+        }
+    }
+    void SetScore()
+    {
+        score.text = "Score: " + scoreValue;
+    }
+
+    void endGame()
+    {
+        if (scoreValue > 1)
+        {
+            SceneManager.LoadScene("End Screen");
+            
+            Debug.Log("endgame");
         }
     }
 }
